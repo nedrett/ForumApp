59b4f72acffc6b11f8272c05613427e9ca391492
nedrett <nedret88@gmail.com>
2022-10-03 18:26:10 +0300

    Initial create and Database Model Post created

---
+++ ForumApp/Data/Models/Post.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumApp.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.EntityFrameworkCore;
+    using static ForumApp.Constants.DataConstants.Post;
+
+    [Comment("Published posts")]
+    public class Post
+    {
+        [Key]
+        [Comment("Posts Identifier")]
+        public int Id { get; set; }
+
+        [Comment("Post title")]
+        [Required]
+        [MaxLength(TitleMaxLength)]
+        public string Title { get; set; } = null!;
+
+        [Comment("Content")]
+        [Required]
+        [MaxLength(ContentMaxLength)]
+        public string Content { get; set; } = null!;
+    }
+}
