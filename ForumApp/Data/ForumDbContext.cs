df2834bacfffdab13dae8752bbe4eb4b03feb3e7
nedrett <nedret88@gmail.com>
2022-10-04 04:50:42 +0300

    Connected to MSSQL Database

---
+++ ForumApp/Data/ForumDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace ForumApp.Data
+{
+    using Configure;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class ForumDbContext : DbContext
+    {
+        public ForumDbContext(DbContextOptions<ForumDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new PostConfiguration());
+
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
--- ForumApp/Program.cs
+++ ForumApp/Program.cs
@@ -1,4 +1,12 @@
+using ForumApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ForumDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
