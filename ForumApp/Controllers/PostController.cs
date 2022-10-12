5e6d30a619aa53e6362b78f8cc89c982b128013f
nedrett <nedret88@gmail.com>
2022-10-12 10:33:53 +0300

    Add PostViewModel and Posts page

---
+++ ForumApp/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ForumApp.Controllers
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Models;
+
+    public class PostController : Controller
+    {
+        private readonly ForumDbContext context;
+
+        public PostController(ForumDbContext _context)
+        {
+            context = _context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await context.Posts
+                .Select(p => new PostViewModel()
+                {
+                    Id = p.Id,
+                    Title = p.Title,
+                    Content = p.Content
+                })
+                .ToListAsync();
+
+            return View(model);
+        }
+    }
+}
---
+++ ForumApp/Models/PostViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ForumApp.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class PostViewModel
+    {
+        [UIHint("hidden")]
+        public int Id { get; set; }
+
+        [Display(Name = "Заглавие")]
+        [Required(ErrorMessage = "Полето е задължително")]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Полето {0} трябва да е между {2} и {1} символа")]
+        public string Title { get; set; }
+
+        [Display(Name = "Съдържание")]
+        [Required(ErrorMessage = "Полето е задължително")]
+        [StringLength(1500, MinimumLength = 30, ErrorMessage = "Полето {0} трябва да е между {2} и {1} символа")]
+        public string Content { get; set; }
+    }
+}
